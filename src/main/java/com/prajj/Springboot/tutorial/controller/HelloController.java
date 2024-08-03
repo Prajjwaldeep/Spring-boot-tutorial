deaaa37992bfcfaf5ebc78b2a71eac0eb14133a4
Prajjwaldeep Ghosh <prajjwaldeepghosh.leo@gmail.com>
2024-08-03 19:48:45 +0530

    RestController

--- src/main/java/com/prajj/Springboot/tutorial/controller/HelloController.java
+++ src/main/java/com/prajj/Springboot/tutorial/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.prajj.Springboot.tutorial.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping("/")
     public String HelloWorld(){
         return "Welcome to Prajj's Spring Boot.";
     }
